6acace4acb7967fa555fef141c058ee1419fec40
mohamedshafikmohamed <48866403+mohamedshafikmohamed@users.noreply.github.com>
2020-05-12 01:28:15 +0200

    Add project files.

---
+++ Ze-Online Learning/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ze_Online_Learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
